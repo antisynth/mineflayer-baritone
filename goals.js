5594a497d80b3a4ad36fad0999eaf0d6d232186c
mat <27899617+mat-1@users.noreply.github.com>
2020-12-11 23:43:59 -0600

    start adding GoalReach

--- goals.js
+++ goals.js
@@ -37,4 +37,27 @@ class GoalBlock extends Goal {
 	}
 }
 
-module.exports = { Goal, GoalBlock }
+class GoalReach extends Goal {
+	constructor(x, y, z) {
+		super()
+		if (x && !y && !z)
+			this.pos = x.offset(.5, 0, .5)
+		else
+			this.pos = new Vec3(x + .5, y, z + .5)
+	}
+	
+	
+	heuristic(node) {
+		return node.distanceTo(this.pos)
+	}
+	
+	isEnd(node) {
+		return visiblePosition(node, this.pos, true)
+	}
+
+	equals(node) {
+		return node.equals(this.pos)
+	}
+}
+
+module.exports = { Goal, GoalBlock, GoalReach }
--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+const { RaycastIterator } = require('mineflayer/lib/iterators')
+
 function isPlayerOnBlock(playerPosition, blockPosition, onGround=false) {
 	// returns true if you can stand on the block
 	
@@ -10,6 +12,40 @@ function isPlayerOnBlock(playerPosition, blockPosition, onGround=false) {
 
 	const onBlock = (xDistance < .7 && zDistance < .7 && yDistance < 1) || (onGround && xDistance < .8 && zDistance < .8 && yDistance == 0)
 	return onBlock
+}
+
+function getViewDirection (pitch, yaw) {
+	const csPitch = Math.cos(pitch)
+	const snPitch = Math.sin(pitch)
+	const csYaw = Math.cos(yaw)
+	const snYaw = Math.sin(yaw)
+	return new Vec3(-snYaw * csPitch, snPitch, -csYaw * csPitch)
+}
+
+function blockIsNotEmpty(pos) {
+	const block = bot.blockAt(pos, false)
+	return block !== null && block.boundingBox !== 'empty'
+}
+
+function canReach(startPosition, endPosition, maxDistance=3) {
+	let stepSize = endPosition.minus(startPosition)
+	let distance = Math.sqrt(stepSize.x * stepSize.x + stepSize.y * stepSize.y + stepSize.z * stepSize.z)
+	if (distance > maxDistance) return false
+	stepSize = stepSize.scaled(1 / distance)
+	stepSize = stepSize.scaled(1 / 5)
+	const totalSteps = distance * 5
+	let stepPosition
+	let previousPosition = startPosition
+	for (let i = 1; i < totalSteps; i++) {
+		stepPosition = previousPosition.plus(stepSize)
+		// check that blocks don't inhabit the same position
+		if (!stepPosition.floored().equals(previousPosition.floored())) {
+			// check block is not transparent
+			if (blockIsNotEmpty(stepPosition)) return false
+		}
+		previousPosition = stepPosition
+	}
+	return true
 }
 
 // (node) => {
